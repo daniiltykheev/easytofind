7bb1e13642f771a4e3129cf66d486dc449357628
Daniil Tykheev <daniiltykheev@Daniils-MacBook-Pro.local>
2022-12-18 13:28:18 +0700

    fixed padding on mobile header

--- components/Header.jsx
+++ components/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
   return (
     <nav className="w-full sticky top-0 bg-stone-300/70 backdrop-blur transition-colors duration-500 border-b border-b-slate-900/10 supports-backdrop-blur:bg-white/95">
       <div className="mx-auto max-w-3xl px-4 sm:px-0">
-        <div className="flex flex-row sm:items-center sm:justify-between sm:py-2 sm:mx-0">
+        <div className="flex flex-row sm:items-center sm:justify-between pt-4 sm:py-2 sm:mx-0">
           <div className="flex flex-start">
           <Link href={"/"}>
               <span className="py-2 link-animate text-sm sm:text-lg font-medium sm:font-medium hover:opacity-50 text-neutral-700 cursor-pointer">
